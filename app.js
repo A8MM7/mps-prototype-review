6da8acffc675673c0b6e43298c1b4e094fa9f218
MPS Prototype CI <31950637+A8MM7@users.noreply.github.com>
2026-09-15 14:54:22 +0000

    deploy: publish QA-tested 183c0c35c04a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Owner-review copy registry. Keep display wording separate from business actions/state.
 const MPS_UI_COPY = Object.freeze({
-  applicationFamilyWithdrawalAction: 'sweet mommy'
+  applicationFamilyWithdrawalAction: 'Family withdrew'
 });
 
 const ORG = 'Malithi International Preschool';
